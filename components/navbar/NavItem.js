1feab1df2d04c55abca11e5daacc4a08b366e950
Harithya Wisesa <harithya77@gmail.com>
2021-12-24 12:34:55 +0700

    fix-mengubah bg navbar hover

--- components/navbar/NavItem.js
+++ components/navbar/NavItem.js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 export default function NavItem(props) {
     const router = useRouter();
     return (
-        <li className='cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-800'>
+        <li className='cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-900'>
             <Link href={props.href ?? '/'}>
                 <a className={(router.asPath == props.href) ? 'underline' : 'no-underline'}>
                     <span className='xl:py-5 block text-sm font-medium py-3 xl:px-5 px-8'>{props.children}</span>
