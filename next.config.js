fd098632d59a9b6045916e3148add25d028710d7
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2021-12-26 11:27:03 +0700

    log-menambahkan pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    // disable: process.env.NODE_ENV === 'development',
+    // register: true,
+    // scope: '/app',
+    // sw: 'service-worker.js',
+    //...
+  }
+})
