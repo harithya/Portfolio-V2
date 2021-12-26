6584f42c01230b8ff9fdeed0cd37845a88657293
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2021-12-27 06:39:23 +0700

    log-menambahkan meta graph

--- components/MetaSeo.js
+++ components/MetaSeo.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import { NextSeo } from 'next-seo'
+import constant from '../utilities/constant'
 
 export default function MetaSeo(props) {
     return (
@@ -10,6 +11,37 @@ export default function MetaSeo(props) {
                 title={props.title}
                 description={props.description}
             />
+            ;<>
+                <meta name="title" content={props.title} />
+                <meta
+                    name="description"
+                    content={props.description}
+                />
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content={constant.url} />
+                <meta property="og:title" content={props.title} />
+                <meta
+                    property="og:description"
+                    content={props.description}
+                />
+                <meta
+                    property="og:image"
+                    content={"/meta.png"}
+                />
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content={constant.url} />
+                <meta property="twitter:title" content={props.title} />
+                <meta
+                    property="twitter:description"
+                    content={props.description}
+                />
+                <meta
+                    property="twitter:image"
+                    content={"/meta.png"}
+                />
+            </>
         </Head>
     )
 }
--- utilities/constant.js
+++ utilities/constant.js
@@ -1,6 +1,7 @@
 const constant = {
     baseUrl: "https://blooming-taiga-09629.herokuapp.com/api",
-    path: "https://blooming-taiga-09629.herokuapp.com"
+    path: "https://blooming-taiga-09629.herokuapp.com",
+    url: "https://harithya.vercel.app/"
 }
 
 export default constant
