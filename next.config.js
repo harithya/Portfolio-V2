57c74e9744c6fdb390848f6120bbba996fc5c0ac
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2021-12-26 11:35:18 +0700

    delete pwa

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    // disable: process.env.NODE_ENV === 'development',
-    // register: true,
-    // scope: '/app',
-    // sw: 'service-worker.js',
-    //...
-  }
-})
+module.exports = {
+  reactStrictMode: true,
+}
