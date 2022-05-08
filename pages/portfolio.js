221934321ab003a09dacaf43fb2fe94a911255b2
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2022-05-08 09:49:16 +0700

    fix undifine data

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -45,9 +45,8 @@ export default function Portofolio({ portfolios }) {
 }
 
 export async function getStaticProps() {
-
     const req = await axios.get("https://harithya.vercel.app/api/portofolio");
-    const portfolios = req.data;
+    const portfolios = req.data ?? [];
     return {
         props: {
             portfolios
