7ebf0b34d06d76f4ba0bf217d016aaffb613ed72
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2022-05-24 09:43:37 +0700

    remove animmation card

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card(props) {
     return (
-        <div className='border-2 rounded-md cursor-pointer border-dashed' data-aos="fade-up">
+        <div className='border-2 rounded-md cursor-pointer border-dashed'>
             {props.children}
         </div>
     )
