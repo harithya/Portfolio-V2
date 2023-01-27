083c4ae175f5119f236ba7f516c192ebcafc996f
Harithya Wisesa <43434776+harithya@users.noreply.github.com>
2023-01-27 20:29:03 +0700

    mengubah base url

--- utilities/constant.js
+++ utilities/constant.js
@@ -1,7 +1,7 @@
 const constant = {
-    baseUrl: "http://localhost/portfolio/api/content",
+    baseUrl: "https://portfolio.haruthya.my.id/api/content",
     path: "https://blooming-taiga-09629.herokuapp.com",
-    storage: "http://localhost/portfolio/storage/uploads/",
+    storage: "https://portfolio.haruthya.my.id/storage/uploads/",
     url: "https://harithya.vercel.app/",
     keyword: 'Harithya Wisesa,Programmer Tasik, Haritya, Haritia, Frontend Developer, Developer, Programmer, Tasikmalaya',
     key: "API-6a9fcbb10229b34e167354e6dd1a698dbafc04ee"
